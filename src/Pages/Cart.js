763aec398ef023e48231c5a62bd158eff10bba09
Your Name <you@example.com>
2022-02-08 14:47:51 +0700

    update

---
+++ src/Pages/Cart.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class Cart extends Component {
+    render() {
+        return <div>
+            <h1 style={{ textAlign: 'center', paddingTop: '10px' }}>GIỎ HÀNG</h1>
+            <hr />
+        </div>;
+    }
+}
